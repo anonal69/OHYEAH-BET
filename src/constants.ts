8ab93d3a10259d4502f78a24a56d251054355646
kuyol-project <153427860+BUILDPROJECT222@users.noreply.github.com>
2024-02-17 12:36:06 +0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,13 +2,13 @@ import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 import { PublicKey } from '@solana/web3.js'
 
 // Can be configured in .env
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com"
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://candace-x68syf-fast-mainnet.helius-rpc.com/"
 
 // Change this value to your Solana address
-export const PLATFORM_CREATOR_ADDRESS = new PublicKey('V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9')
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey('7s3nxNnKcmJyBxxXeJb8AAJnRocJLCRhbrCcYhPKv5SP')
 
 // Appears in ShareModal
-export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
+export const PLATFORM_SHARABLE_URL = 'OH YEAH!!'
 
 // List of tokens supported by this platform
 export const TOKENS: TokenMeta[] = [
