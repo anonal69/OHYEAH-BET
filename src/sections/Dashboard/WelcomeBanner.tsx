83cb593be3ac5b43a996b57633b86b310039e52e
anonal69 <157854508+anonal69@users.noreply.github.com>
2024-02-18 17:07:12 +0700

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -19,7 +19,7 @@ export function WelcomeBanner() {
   return (
     <Welcome>
       <div className="image-container">
-        <Image src=".\src\OHYEAHbet.png" alt="OHYEAH Banner" />
+        <Image src="/IMG_1794.png" alt="OHYEAH Banner" />
       </div>
       <div className="button-container">
         <Buttons>
