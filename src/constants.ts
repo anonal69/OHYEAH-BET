27af69a47033ccb8ff4bb6e6bbb6ca5ad1a11a2e
anonal69 <157854508+anonal69@users.noreply.github.com>
2024-02-21 04:40:44 +0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import { PublicKey } from '@solana/web3.js'
 export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://candace-x68syf-fast-mainnet.helius-rpc.com/"
 
 // Change this value to your Solana address
-export const PLATFORM_CREATOR_ADDRESS = new PublicKey('7s3nxNnKcmJyBxxXeJb8AAJnRocJLCRhbrCcYhPKv5SP')
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey('Eh8Qi5C5tyMwvEs8UxwcoJG3ag2Yy2jVFPRX472yUKPu')
 
 // Appears in ShareModal
 export const PLATFORM_SHARABLE_URL = 'OH YEAH!!'
